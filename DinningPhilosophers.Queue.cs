6d759ae919f5a8e3bddb885a1662b7863cf4e1e8
AK <artyomkarpov@gmail.com>
2019-04-19 17:54:50 +0300

    save

---
+++ DinningPhilosophers.Queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DPProblem
+{
+	public partial class DinningPhilosophers : IDisposable
+	{
+		class AgentsSolution
+		{
+			private enum EMessages
+			{
+				Hungry,
+				TakeFork,
+			}
+
+			private BlockingCollection<EMessages>[] _inboxes;
+			public AgentsSolution()
+			{
+				_inboxes = new BlockingCollection<EMessages>[PhilosophersAmount];
+				for (int i = 0; i < PhilosophersAmount; i++)
+					_inboxes[i] = new BlockingCollection<EMessages>(new ConcurrentQueue<EMessages>());
+			}
+
+			public void Run(int i, CancellationToken token)
+			{
+				var watch = new Stopwatch();
+				while (true)
+				{
+					watch.Restart();
+					await TakeForks(i);
+					_waitTime[i] += watch.ElapsedMilliseconds;
+
+					eatenFood[i] = (eatenFood[i] + 1) % (int.MaxValue - 1);
+
+					watch.Restart();
+					await PutForks(i);
+					_waitTime[i] += watch.ElapsedMilliseconds;
+
+					Think(i);
+
+					if (token.IsCancellationRequested) break;
+				}
+
+			}
+		}
+	}
+}
